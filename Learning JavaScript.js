dc2e9a329099e2dc19b3fbe00e586f7e58311c5c
DevEkpereamaka-cloud <spongebobmage@gmail.com>
2026-02-03 17:58:59 +0100

    Add files via upload

--- Learning JavaScript.js
+++ Learning JavaScript.js
@@ -1,16 +1,3 @@
-var newLine =
-  "\tMy name is Ekpereamaka\n\ti am lerning javascript\n\tthis is the third line";
-console.log(newLine);
-var myString = "this is the first line ";
-myString += "\nthis is the second line";
-console.log(myString);
-var myName = "Ekpereamaka";
-var myStr = "Hello my name is " + myName + " and i am learning JavaScript";
-console.log(myStr);
-var name = "JavaScript";
-var nameLength = name.length;
-console.log(nameLength);
-
 function wordBrackets(aNoun, anAdjective, aVerb, anadverb) {
   let result = " ";
   result +=
@@ -27,3 +14,7 @@ function wordBrackets(aNoun, anAdjective, aVerb, anadverb) {
 }
 
 console.log(wordBrackets("Dog", "Big", "chased", "immediately"));
+
+let myArray = ["I am a ", " backend developer "];
+myArray.push([" in training"]);
+console.log(myArray);
