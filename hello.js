cdef177d5bbff4f6e3a2f2b3f1cacb77efee1ad1
DevEkpereamaka-cloud <spongebobmage@gmail.com>
2026-02-03 12:55:31 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 var newLine =
-  "\tMy name is Ekpereamaka\n\ti am lerning javascript\n\tthis is the third line";
+  "\tMy name is Ekpereamaka\n\ti am learning javascript\n\tthis is the third line";
 console.log(newLine)
+
