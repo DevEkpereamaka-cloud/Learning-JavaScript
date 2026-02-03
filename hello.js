012ade3216f8fe6516ad75e1c58fd4649997ca45
DevEkpereamaka-cloud <spongebobmage@gmail.com>
2026-02-03 13:21:16 +0100

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,4 +1,9 @@
 var newLine =
-  "\tMy name is Ekpereamaka\n\ti am learning javascript\n\tthis is the third line";
-console.log(newLine)
-
+  "\tMy name is Ekpereamaka\n\ti am lerning javascript\n\tthis is the third line";
+console.log(newLine);
+var myString = "this is the first line ";
+myString += "\nthis is the second line";
+console.log(myString);
+var myName = "Ekpereamaka";
+var myStr = "Hello my name is " + myName + " and i am learning JavaScript";
+console.log(myStr);
