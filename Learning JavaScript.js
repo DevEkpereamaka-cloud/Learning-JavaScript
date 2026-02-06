118cf04f61ac036ec93152cfc1d7f192a3696586
DevEkpereamaka-cloud <spongebobmage@gmail.com>
2026-02-06 14:08:42 +0100

    Add files via upload

--- Learning JavaScript.js
+++ Learning JavaScript.js
@@ -16,5 +16,26 @@ function wordBrackets(aNoun, anAdjective, aVerb, anadverb) {
 console.log(wordBrackets("Dog", "Big", "chased", "immediately"));
 
 let myArray = ["I am a ", " backend developer "];
-myArray.push([" in training"]);
+myArray.push(" in training");
 console.log(myArray);
+let ourArray = [["Knowledge"], ["Strength"], ["Discipline"], ["Laziness"]];
+let badArray = ourArray.pop();
+console.log(ourArray);
+console.log(badArray);
+
+function kilimanjaro(bakery, restaurant) {
+  bakery.push(restaurant);
+  return bakery.shift();
+}
+let theQueue = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log("in the morning: " + JSON.stringify(theQueue));
+console.log(kilimanjaro(theQueue, 9));
+console.log("in the afternoon: " + JSON.stringify(theQueue));
+
+let myStack = [10, 20, 30, 40];
+function removedItem() {
+  return myStack.shift();
+}
+myStack.push(50);
+console.log("myStack: " + JSON.stringify(myStack));
+console.log(removedItem());
