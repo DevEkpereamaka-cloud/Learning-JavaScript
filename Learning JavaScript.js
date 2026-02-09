26e15fb0b3e2552f0f6bda6be785ca565b54bd56
DevEkpereamaka-cloud <spongebobmage@gmail.com>
2026-02-09 15:40:44 +0100

    Add files via upload

--- Learning JavaScript.js
+++ Learning JavaScript.js
@@ -18,6 +18,15 @@ console.log(wordBrackets("Dog", "Big", "chased", "immediately"));
 let myArray = ["I am a ", " backend developer "];
 myArray.push(" in training");
 console.log(myArray);
+
+let myStack = [10, 20, 30, 40];
+function removedItem() {
+  return myStack.shift();
+}
+myStack.push(50);
+console.log("myStack: " + JSON.stringify(myStack));
+console.log("removedItem: " + JSON.stringify(removedItem()));
+
 let ourArray = [["Knowledge"], ["Strength"], ["Discipline"], ["Laziness"]];
 let badArray = ourArray.pop();
 console.log(ourArray);
@@ -32,10 +41,30 @@ console.log("in the morning: " + JSON.stringify(theQueue));
 console.log(kilimanjaro(theQueue, 9));
 console.log("in the afternoon: " + JSON.stringify(theQueue));
 
-let myStack = [10, 20, 30, 40];
-function removedItem() {
-  return myStack.shift();
+let myTray = [
+  ["your with me too", "ate nine"],
+  ["lets plant the tree", "yeah he was forgiven"],
+];
+
+function stillOnSwitch(num) {
+  switch (num) {
+    case 1:
+      return "GOD is #1";
+      break;
+    case 2:
+      return myTray[0][0];
+      break;
+    case 3:
+      return myTray[1][0];
+      break;
+    case 4:
+      return myTray[1][1];
+      break;
+    case 7:
+      return myTray[0][1];
+      break;
+    default:
+      return "haha LOL";
+  }
 }
-myStack.push(50);
-console.log("myStack: " + JSON.stringify(myStack));
-console.log(removedItem());
+console.log(stillOnSwitch(1));
